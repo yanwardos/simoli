e9926064dfb2d76fcb2f6fa9ccb13f8fcea34c05
yanwardo <ywdfsctgclev0301@gmail.com>
2020-09-18 11:42:51 +0700

    update opsi gedung

--- app/Controllers/Monitoring.php
+++ app/Controllers/Monitoring.php
@@ -14,104 +14,120 @@ use Throwable;
 
 class Monitoring extends Controller
 {
-    function __construct()
-    {
-        $this->db = \Config\Database::connect();
-        helper('form', 'url');
-    }
+	function __construct()
+	{
+		$this->db = \Config\Database::connect();
+		helper('form', 'url');
+	}
 
 	public function index()
-	{  
-        $monitorings = new DataMonitoring();
-        $monitorings = $monitorings->asArray()->findAll();
-        
-        $gedungs = new DataGedung();
-        $gedungs = $gedungs->asArray()->findAll();
+	{
+		$monitorings = new DataMonitoring();
+		$monitorings = $monitorings->asArray()->findAll();
 
-        echo view('layout/header');
-        echo view('monitoring/index', [
-            'monitorings'=>$monitorings,
-            'gedungs' => $gedungs
-        ]);
-        echo view('layout/footer');
-    }
+		$gedungs = new DataGedung();
+		$gedungs = $gedungs->asArray()->findAll();
 
-    public function grafik()
-    {
-        $gedungs = new DataGedung();
-        $gedungs = $gedungs->asArray()->findAll();
+		echo view('layout/header');
+		echo view('monitoring/index', [
+					  'monitorings' => $monitorings,
+					  'gedungs'     => $gedungs,
+				  ]);
+		echo view('layout/footer');
+	}
 
-        echo view('layout/header');
-        echo view('monitoring/grafik',[
-            'gedungs' => $gedungs
-        ]);
-        echo view('layout/footer');
-    }
-    
-    public function tambahIface()
-    {
-        $gedungs = new DataGedung();
-        $gedungs = $gedungs->asArray()->findAll();
-        
-        echo view('layout/header');
-        echo view('monitoring/tambah', [
-            'gedungs' => $gedungs
-        ]);
-        echo view('layout/footer');
-    }
+	public function grafik()
+	{
+		$gedungs = new DataGedung();
+		$gedungs = $gedungs->asArray()->findAll();
 
-    public function tambah()
-    {
-        try {
-            $waktu =  $this->request->getPost('waktu-tanggal')." ".$this->request->getPost('waktu-jam');
-            $data['id_gedung']  = $this->request->getPost('gedung');
-            $data['waktu_rekord']   = $waktu;
-            $data['tegangan'] = $this->request->getPost('tegangan');
-            $data['kwh'] = $this->request->getPost('kwh');
-            $data['arus'] = $this->request->getPost('arus');
-            $data['frekuensi'] = $this->request->getPost('frekuensi');
-            $data['daya_aktif'] = $this->request->getPost('daya_aktif');
-            $data['daya_tampak'] = $this->request->getPost('daya_tampak');
-        } catch (\Throwable $th) {
-            return $th->getMessage();
-        }
+		echo view('layout/header');
+		echo view('monitoring/grafik', [
+					  'gedungs' => $gedungs,
+				  ]);
+		echo view('layout/footer');
+	}
 
-        try {
-            $rekord = new DataMonitoring();
-            $cek = $rekord->insert($data);
-            return redirect()->to('/monitoring');
-        } catch (\Throwable $th) {
-            return $th->getMessage();
-        }
-    }
+	public function tambahIface()
+	{
+		$gedungs = new DataGedung();
+		$gedungs = $gedungs->asArray()->findAll();
 
-    public function getDataMonitoring(){
-        $start = $this->request->getGet('startDate');
-        $end = $this->request->getGet('endDate');
-        $idGedung = $this->request->getGet('idGedung');
+		echo view('layout/header');
+		echo view('monitoring/tambah', [
+					  'gedungs' => $gedungs,
+				  ]);
+		echo view('layout/footer');
+	}
 
-        try {
-            $start=new DateTime($start);
-            $start=$start->format('Y-m-d H:m:i');
-        } catch (\Throwable $th) {
-            $start=false;
-        }
+	public function tambah()
+	{
+		try
+		{
+			$waktu                = $this->request->getPost('waktu-tanggal') . ' ' . $this->request->getPost('waktu-jam');
+			$data['id_gedung']    = $this->request->getPost('gedung');
+			$data['waktu_rekord'] = $waktu;
+			$data['tegangan']     = $this->request->getPost('tegangan');
+			$data['kwh']          = $this->request->getPost('kwh');
+			$data['arus']         = $this->request->getPost('arus');
+			$data['frekuensi']    = $this->request->getPost('frekuensi');
+			$data['daya_aktif']   = $this->request->getPost('daya_aktif');
+			$data['daya_tampak']  = $this->request->getPost('daya_tampak');
+		}
+		catch (\Throwable $th)
+		{
+			return $th->getMessage();
+		}
 
-        try{
-            $end=new DateTime($end);
-            $end=$end->format('Y-m-d H:m:i');
-        } catch (\Throwable $th){
-            $end=false;
-        }
+		try
+		{
+			$rekord = new DataMonitoring();
+			$cek    = $rekord->insert($data);
+			return redirect()->to('/monitoring');
+		}
+		catch (\Throwable $th)
+		{
+			return $th->getMessage();
+		}
+	}
 
-        if(!$start || !$end){
-            $query = $this->db->query('SELECT * FROM data_monitoring_listrik WHERE id_gedung='.$idGedung.' ORDER BY waktu_rekord ASC');
-        }else{
-            $query = $this->db->query('SELECT * FROM data_monitoring_listrik WHERE (id_gedung='.$idGedung.') and (waktu_rekord BETWEEN '."'".$start."'"." and "."'".$end."'".") ORDER BY waktu_rekord ASC");
-        }
+	public function getDataMonitoring()
+	{
+		$start    = $this->request->getGet('startDate');
+		$end      = $this->request->getGet('endDate');
+		$idGedung = $this->request->getGet('idGedung');
 
-        $result = $query->getResult();
-        $this->response->setJSON($result, true);
-        print_r(json_encode($result));
-    }
+		try
+		{
+			$start = new DateTime($start);
+			$start = $start->format('Y-m-d H:m:i');
+		}
+		catch (\Throwable $th)
+		{
+			$start = false;
+		}
+
+		try
+		{
+			$end = new DateTime($end);
+			$end = $end->format('Y-m-d H:m:i');
+		}
+		catch (\Throwable $th)
+		{
+			$end = false;
+		}
+
+		if (! $start || ! $end)
+		{
+			$query = $this->db->query('SELECT * FROM data_monitoring_listrik WHERE id_gedung=' . $idGedung . ' ORDER BY waktu_rekord ASC');
+		}
+		else
+		{
+			$query = $this->db->query('SELECT * FROM data_monitoring_listrik WHERE (id_gedung=' . $idGedung . ') and (waktu_rekord BETWEEN ' . "'" . $start . "'" . ' and ' . "'" . $end . "'" . ') ORDER BY waktu_rekord ASC');
+		}
+
+		$result = $query->getResult();
+		$this->response->setJSON($result, true);
+		print_r(json_encode($result));
+	}
 }
--- app/Views/monitoring/grafik.php
+++ app/Views/monitoring/grafik.php
@@ -1,111 +1,115 @@
 <div class="container-fluid">
-    <div class="row p-2">
-        <div class="col-lg-8 col-md-12 col-sm-12">
-            <canvas id="chart" class="w-100"></canvas>
-            <hr>
-        </div>
-        <div class="col-lg-4 col-md-12 col-sm-12">
-            <div class="row">
-                <div class="col">
-                    <label for="id-gedung">Gedung</label>
-                    <select class="form-control" name="id-gedung" id="id-gedung">
-                        <?php foreach ($gedungs as $key => $value) {?>
-                            <option value="<?php echo $value['id_gedung']?>"><?php echo $value['nama_gedung']?></option>
-                        <?php } ?>
-                    </select>
-                    <hr>
-                    <label for="start-date">Start Date</label>
-                    <input class="form-control" type="date" name="start-date" id="start-date">
-                    <hr>
-                    <label for="end-date">End Date</label>
-                    <input class="form-control" type="date" name="end-date" id="end-date">
-                </div>
-                <div class="col">
-                    
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="row p-2">
+		<div class="col-lg-8 col-md-12 col-sm-12">
+			<canvas id="chart" class="w-100"></canvas>
+			<hr>
+		</div>
+		<div class="col-lg-4 col-md-12 col-sm-12">
+			<div class="row">
+				<div class="col">
+					<label for="id-gedung">Gedung</label>
+					<select class="form-control" name="id-gedung" id="id-gedung">
+						<?php
+						foreach ($gedungs as $key => $value)
+						{
+							?>
+							<option value="<?php echo $value['id_gedung']?>"><?php echo $value['nama_gedung']?></option>
+							<?php
+						} ?>
+					</select>
+					<hr>
+					<label for="start-date">Start Date</label>
+					<input class="form-control" type="date" name="start-date" id="start-date">
+					<hr>
+					<label for="end-date">End Date</label>
+					<input class="form-control" type="date" name="end-date" id="end-date">
+				</div>
+				<div class="col">
+					
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 <script>
-    var startDate = document.getElementById('start-date');
-    var endDate = document.getElementById('end-date');
-    var idGedung = document.getElementById('id-gedung');
-    
-    startDate.onchange = function(){
-        if(startDate.value!="" && endDate.value!="") updateGrafik(idGedung.value, startDate.value, endDate.value);
-    }
+	var startDate = document.getElementById('start-date');
+	var endDate = document.getElementById('end-date');
+	var idGedung = document.getElementById('id-gedung');
+	
+	startDate.onchange = function(){
+		if(startDate.value!="" && endDate.value!="") updateGrafik(idGedung.value, startDate.value, endDate.value);
+	}
 
-    endDate.onchange = function(){
-        if(startDate.value!="" && endDate.value!="") updateGrafik(idGedung.value, startDate.value, endDate.value);
-    }
+	endDate.onchange = function(){
+		if(startDate.value!="" && endDate.value!="") updateGrafik(idGedung.value, startDate.value, endDate.value);
+	}
 
-    idGedung.onchange = function(){
-        if(startDate.value!="" && endDate.value!="") updateGrafik(idGedung.value, startDate.value, endDate.value);
-    }
+	idGedung.onchange = function(){
+		if(startDate.value!="" && endDate.value!="") updateGrafik(idGedung.value, startDate.value, endDate.value);
+	}
 
-    window.onload = function(){
-        updateGrafik();
-    }
+	window.onload = function(){
+		updateGrafik();
+	}
 
-    function updateGrafik(idGedung=false, startDate=false, endDate=false){
+	function updateGrafik(idGedung=false, startDate=false, endDate=false){
 
-        getData(idGedung, startDate, endDate, function(data){
-            jams = Array();
-            kwhs = Array();
-            data.forEach(element => {
-                date = Date.parse(element.waktu_rekord);
-                date = new Date(date);
-                element.waktu = {
-                    hari: date.getDay(),
-                    tanggal: date.getDate(),
-                    bulan: date.getMonth(),
-                    tahun: date.getFullYear(),
-                    jam: date.getHours()+":"+date.getMinutes()
-                }
+		getData(idGedung, startDate, endDate, function(data){
+			jams = Array();
+			kwhs = Array();
+			data.forEach(element => {
+				date = Date.parse(element.waktu_rekord);
+				date = new Date(date);
+				element.waktu = {
+					hari: date.getDay(),
+					tanggal: date.getDate(),
+					bulan: date.getMonth(),
+					tahun: date.getFullYear(),
+					jam: date.getHours()+":"+date.getMinutes()
+				}
 
-                jams.push(element.waktu.jam);
-                kwhs.push(element.kwh)
-            });
+				jams.push(element.waktu.jam);
+				kwhs.push(element.kwh)
+			});
 
-            var ctx = document.getElementById('chart').getContext('2d');
-            var chart = new Chart(ctx, {
-                // The type of chart we want to create
-                type: 'line',
+			var ctx = document.getElementById('chart').getContext('2d');
+			var chart = new Chart(ctx, {
+				// The type of chart we want to create
+				type: 'line',
 
-                // The data for our dataset
-                data: {
-                    labels: jams,
-                    datasets: [{
-                        label: 'KWh',
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: kwhs
-                    }]
-                },
+				// The data for our dataset
+				data: {
+					labels: jams,
+					datasets: [{
+						label: 'KWh',
+						backgroundColor: 'rgb(255, 99, 132)',
+						borderColor: 'rgb(255, 99, 132)',
+						data: kwhs
+					}]
+				},
 
-                // Configuration options go here
-                options: {}
-            });
-        })
-    }
+				// Configuration options go here
+				options: {}
+			});
+		})
+	}
 
-    function getData(idGedung, startDate, endDate, onSucces){
-        $.ajax({
-            type  : 'GET',
-            url   : '<?php base_url()?>/datamonitoring',
-            data:{
-                idGedung: idGedung,
-                startDate: startDate,
-                endDate: endDate
-            },
-            async : true,
-            dataType : 'json',
-            success : function(data){
-                onSucces(data);
-            }
-        });
-    }
+	function getData(idGedung, startDate, endDate, onSucces){
+		$.ajax({
+			type  : 'GET',
+			url   : '<?php base_url()?>/datamonitoring',
+			data:{
+				idGedung: idGedung,
+				startDate: startDate,
+				endDate: endDate
+			},
+			async : true,
+			dataType : 'json',
+			success : function(data){
+				onSucces(data);
+			}
+		});
+	}
 </script>
