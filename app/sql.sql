6e68781fa14e0737069506ceb4cee91007759b6c
yanwardo <ywdfsctgclev0301@gmail.com>
2020-09-22 21:07:20 +0700

    sql

--- app/sql.sql
+++ app/sql.sql
@@ -1,30 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6deb5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Sep 22, 2020 at 08:38 PM
--- Server version: 5.7.31-0ubuntu0.18.04.1
--- PHP Version: 7.2.33-1+ubuntu18.04.1+deb.sury.org+1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `monitoring_listrik_2`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `data_monitoring`
---
 
 CREATE TABLE `data_monitoring` (
   `id_rekord` bigint(20) NOT NULL,
